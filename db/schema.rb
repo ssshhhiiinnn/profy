167a341aa412e1bca981ee78edcebf81d3eb1a90
Shinnosuke Azuma <shinnosukeazuma@Shinnosuke-no-MacBook-Pro.local>
2017-10-20 09:39:16 +0900

    ユーザーの新規登録の際に、group_idが保存されるようにuser.rbを編集

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019122436) do
+ActiveRecord::Schema.define(version: 20171019122804) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "key",        limit: 255
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20171019122436) do
     t.string   "unconfirmed_email",      limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.integer  "group_id",               limit: 4
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
