22823e0cbed705b27672209cf493407fe93ec0d4
Shinnosuke Azuma <shinnosukeazuma@Shinnosuke-no-MacBook-Pro.local>
2017-10-20 11:55:22 +0900

    groupに関して、ルーティング、コントローラ、ビューを定義した

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show, :edit, :update]
   root to: "top#index"
+  resources :users, only: [:show, :edit, :update]
+  resources :groups, only: [:show, :edit, :update]
 
 end
